c752ad80e093a2e32196e639de23e69adcb5e87b
TilakDixit <119035431+TilakDixit@users.noreply.github.com>
2024-07-23 08:52:54 +0530

    Create next-greater-element-i.java

---
+++ next-greater-element-i.java
@@ -0,0 +1,18 @@
+  Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Fill the map with next greater elements for nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Prepare the result array for nums1 using the map
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
