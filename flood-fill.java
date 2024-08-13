f76ba74b7f7444360f38c77db8454bf002bf950d
TilakDixit <119035431+TilakDixit@users.noreply.github.com>
2024-08-14 00:03:52 +0530

    Create flood-fill.java

---
+++ flood-fill.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int originalColor = image[sr][sc];
+        if (originalColor != newColor) {
+            dfs(image, sr, sc, originalColor, newColor);
+        }
+        return image;
+    }
+
+    private void dfs(int[][] image, int x, int y, int originalColor, int newColor) {
+        if (x < 0 || x >= image.length || y < 0 || y >= image[0].length || image[x][y] != originalColor) {
+            return;
+        }
+        image[x][y] = newColor;
+        dfs(image, x + 1, y, originalColor, newColor);
+        dfs(image, x - 1, y, originalColor, newColor);
+        dfs(image, x, y + 1, originalColor, newColor);
+        dfs(image, x, y - 1, originalColor, newColor);
+    }
+}
