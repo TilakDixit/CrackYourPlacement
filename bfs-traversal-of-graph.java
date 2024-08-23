3d988d86704249f82460a0f9083844fe20c82e68
TilakDixit <119035431+TilakDixit@users.noreply.github.com>
2024-08-23 11:45:55 +0530

    Create bfs-traversal-of-graph.java

---
+++ bfs-traversal-of-graph.java
@@ -0,0 +1,28 @@
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+     
+    
+        ArrayList<Integer> ap = new ArrayList<>();
+        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(0);
+        visited[0] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            ap.add(node);
+
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+
+        return ap;
+    }
+}
